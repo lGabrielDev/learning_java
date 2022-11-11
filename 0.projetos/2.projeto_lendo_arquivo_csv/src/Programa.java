2b8c90a9e36dcde3a34aeef12385f81b7425ee22
lGabrielDev <thegabrielfreitasbf@yahoo.com.br>
2022-11-11 17:20:25 -0300

    Atualizado resumos sobre 'arquivo csv'

--- 0.projetos/2.projeto_lendo_arquivo_csv/src/Programa.java
+++ 0.projetos/2.projeto_lendo_arquivo_csv/src/Programa.java
@@ -7,13 +7,17 @@ import java.util.List;
 public class Programa {
     public static void main(String[] args){
         String caminho = "C:/Users/Gabriel/Desktop/praticando.csv";
+        File arquivo = new File(caminho);
 
         List<Usuario> lista = new ArrayList<>();
         try{
-            BufferedReader br = new BufferedReader(new FileReader(caminho));
+            BufferedReader br = new BufferedReader(new FileReader(arquivo));
+
+            //removendo o cabeçalho do arquivo
             String linha = br.readLine();
             linha = br.readLine();
 
+            //lendo o arquivo completo
             while(linha != null){
                 String[] informacoes = linha.split(";");
                 String name = informacoes[0];
